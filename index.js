e4a9a16b577cf9a3a421fc7e1b5d550f2eb9b310
gartemy <artemganzhaa@gmail.com>
2021-08-22 23:48:59 +0500

    Arrow-up commit

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-  $(".navbar-toggler").on("click", function () {
-    $(".animated-icon").toggleClass("open");
+  // Mobile Menu animation (open)
+  $('.navbar-toggler').on('click', function () {
+    $('.animated-icon').toggleClass('open');
+  });
+
+  // Mobile Menu animation (hide)
+  $('.nav-link').on('click', function () {
+    $('.animated-icon').removeClass('open');
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Arrow-up animation
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 812) {
+      $('.arrow-up').css('display', 'flex');
+      $('.arrow-up').fadeIn();
+    } else {
+      $('.arrow-up').fadeOut();
+    }
   });
 });
